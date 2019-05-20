bee3606ec38543cd9d29e2aa2740e264be9c0b56
ZKiers <zoe-kiers@outlook.com>
2019-05-20 18:50:48 +0200

    Add tree house outside (use "go up" outside to access).

--- ZuulCS_Zoe_Kiers/Game.cs
+++ ZuulCS_Zoe_Kiers/Game.cs
@@ -16,7 +16,7 @@ namespace ZuulCS
 
 		private void createRooms()
 		{
-			Room outside, theatre, pub, lab, office;
+			Room outside, theatre, pub, lab, office, treeHouse;
 
 			// create the rooms
 			outside = new Room("outside the main entrance of the university");
@@ -24,11 +24,13 @@ namespace ZuulCS
 			pub = new Room("in the campus pub");
 			lab = new Room("in a computing lab");
 			office = new Room("in the computing admin office");
+            treeHouse = new Room("in the secret tree house built by the seniors");
 
 			// initialise room exits
 			outside.setExit("east", theatre);
 			outside.setExit("south", lab);
 			outside.setExit("west", pub);
+            outside.setExit("up", treeHouse);
 
 			theatre.setExit("west", outside);
 
@@ -38,6 +40,8 @@ namespace ZuulCS
 			lab.setExit("east", office);
 
 			office.setExit("west", lab);
+
+            treeHouse.setExit("down", outside);
 
 			player.setCurrentRoom(outside);  // start game outside
 		}
